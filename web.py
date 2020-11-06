04bb4c49ffaa68b3cf2995a29157c327546ea19e
Vinicius Herrera <vini.hsilva@hotmail.com>
2020-11-05 23:35:29 -0300

    Adicionado busca de proximos filmes

--- web.py
+++ web.py
@@ -10,7 +10,47 @@ app.config['DEBUG'] = True
 @app.route('/api/v1/filmes', methods=['GET'])   # Define a rota para chamar a função
 def filmes():
     url = 'http://www.adorocinema.com/filmes/numero-cinemas/'
-    #url = "http://www.adorocinema.com/filmes/todos-filmes/notas-espectadores/"
+
+    htmlDoc = urlopen(url).read()
+    soup = BeautifulSoup(htmlDoc,'html.parser')    # Converte a página e atribui em uma variável
+    data = []
+
+    for dataBox in soup.findAll('li',class_='mdl'):
+        titleobj = dataBox.find('a',class_='meta-title-link').string
+        #imgObj = dataBox.find(class_='thumbnail-img')['src']
+        dateObj = dataBox.find('div',class_='meta-body-info').find('span',class_='date').string
+        sinopseObj = dataBox.find('div',class_='content-txt').text.replace('\n','').strip()
+
+        movieLinkObj = dataBox.find('a',class_='meta-title-link')['href']
+        detailsLink = 'http://www.adorocinema.com' + movieLinkObj
+        
+        generoObj = dataBox.find('div',class_='meta-body-info').find_all('span')[3:]
+
+        generoList = []
+        for genero in generoObj:
+            generoList.append(genero.text.strip())
+
+        # Load full sinopse
+        htmlDocMovieDetail = urlopen(detailsLink).read()
+        soupMovieDetail = BeautifulSoup(htmlDocMovieDetail,'html.parser')
+        fullSinopse = soupMovieDetail.find('div',class_='content-txt').text.replace('\n','').strip()
+        imgObj = soupMovieDetail.find(class_='thumbnail-img')['src']
+
+        data.append({
+            'titulo' : titleobj,
+            'poster' : imgObj,
+            'genero' : ', '.join(generoList),
+            'data' : dateObj,
+            'sinopse' : sinopseObj,
+            'link' : detailsLink,
+            'sinopseFull':fullSinopse
+        })
+
+    return jsonify({'filmes':data})
+
+@app.route('/api/v1/filmes/em-breve', methods=['GET'])   # Define a rota para chamar a função
+def filmesNew():
+    url = 'http://www.adorocinema.com/filmes/mais-esperados/'
 
     htmlDoc = urlopen(url).read()
     soup = BeautifulSoup(htmlDoc,'html.parser')    # Converte a página e atribui em uma variável
